680b0425e4909f4b73f33a52ff9973b0725852ee
geek_zz <315998318@qq.com>
2022-01-03 17:27:59 +0800

    第9周作业

---
+++ rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/api/EdorConfirm.java
@@ -0,0 +1,13 @@
+package io.kimmking.rpcfx.api;
+
+import java.util.Map;
+
+public class EdorConfirm {
+
+    //真实提交的方法
+    public boolean submitData(Map mMap)
+    {
+        System.out.println("提交成功");
+        return true;
+    }
+}
---
+++ rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/server/RpcfxInvoker.java
@@ -0,0 +1,74 @@
+package io.kimmking.rpcfx.server;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import io.kimmking.rpcfx.api.EdorConfirm;
+import io.kimmking.rpcfx.api.RpcfxRequest;
+import io.kimmking.rpcfx.api.RpcfxResolver;
+import io.kimmking.rpcfx.api.RpcfxResponse;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RpcfxInvoker {
+
+    private RpcfxResolver resolver;
+
+    public RpcfxInvoker(RpcfxResolver resolver){
+        this.resolver = resolver;
+    }
+
+    public RpcfxResponse invoke(RpcfxRequest request) {
+        RpcfxResponse response = new RpcfxResponse();
+        String serviceClass = request.getServiceClass();
+        Map tMap = new HashMap();
+
+        // 作业1：改成泛型和反射
+        Object service = resolver.resolve(serviceClass);//this.applicationContext.getBean(serviceClass);
+        //反射,根据获取的class名字查找实现类
+        Class tClass = null;
+        try {
+            tClass = Class.forName(serviceClass);
+            try {
+                EdorConfirm tEdorConfirm = (EdorConfirm) tClass.newInstance();
+
+                if (!tEdorConfirm.submitData(tMap)) {
+                    return null;
+                }
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Method method = resolveMethodFromClass(service.getClass(), request.getMethod());
+            Object result = method.invoke(service, request.getParams()); // dubbo, fastjson,
+            // 两次json序列化能否合并成一个
+            response.setResult(JSON.toJSONString(result, SerializerFeature.WriteClassName));
+            response.setStatus(true);
+            return response;
+        } catch ( IllegalAccessException | InvocationTargetException e) {
+
+            // 3.Xstream
+
+            // 2.封装一个统一的RpcfxException
+            // 客户端也需要判断异常
+            e.printStackTrace();
+            response.setException(e);
+            response.setStatus(false);
+            return response;
+        }
+    }
+
+    private Method resolveMethodFromClass(Class<?> klass, String methodName) {
+        return Arrays.stream(klass.getMethods()).filter(m -> methodName.equals(m.getName())).findFirst().get();
+    }
+
+}
